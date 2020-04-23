49d959daa744a6132d3bb40d2418d8027230d946
MikeDEV <mikierules109@gmail.com>
2020-04-23 17:09:49 -0400

    thingamigbob

---
+++ server.py
@@ -0,0 +1,38 @@
+#MikeDEV's CloudLink Server
+#Version 1.0
+
+receivedMessages = ""
+
+#Server imports
+import asyncio
+import random
+import websockets
+
+#Data imports
+import json
+
+#Gui imports
+from PyQt5 import QtGui, QtCore
+
+#Address of the other machine it is connected to
+global pairMachine
+
+async def client(websocket, path):
+    global receivedMessages
+    while True:
+        data = await websocket.recv()
+        if data == "%_fetch":
+            #If no messages
+            if receivedMessages == []:
+                await websocket.send("")
+                
+            else:
+                await websocket.send(json.dumps(receivedMessages))
+        else:
+            receivedMessages = str(data)
+
+print("MikeDEV's CloudLink Server v1.0\nNow hosting from ws://127.0.0.1:3000/.")
+start_server = websockets.serve(client, '127.0.0.1', 3000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
