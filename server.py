60b4d5ba4ad89af7c97c769f1898f8e6b939b879
MikeDEV <mikierules109@gmail.com>
2020-07-09 19:34:20 -0400

    Update 1.6b
    * Bugfix/rewrite
    * Now supports multiple asynchronous connections with asynchronous data streams

--- server.py
+++ server.py
@@ -1,30 +1,55 @@
-# -*- coding: utf-8 -*-
 #MikeDEV's CloudLink Server
-#Version 1.6
-
-dStream = ""
+#Version 1.6b
 
 import asyncio
-import websockets
 import json
+import websockets
 
-#Primary script that does all of our magic...
-async def client(websocket, path):
-    global dStream
-    await websocket.send(json.dumps(dStream)) # Update the new client with the latest data
-    while True: # Loop forever, relay data between clients
-        try:
-            data = await websocket.recv()
-            print(str(data))
-            dStream = str(data)
-            await websocket.send(json.dumps(dStream))
-        except:
-            pass
+STREAMS = {"ps": ""} #Define data streams, will improve upon this design later
+USERS = set() #create unorganized, non-indexed set of users
 
-# Print version and info :)
-print("MikeDEV's CloudLink Server v1.6\nNow listening for requests on port 3000.\n")
-server = websockets.serve(client, '127.0.0.1', 3000)
+def state_event():
+    return json.dumps(str(STREAMS["ps"]))
 
-# Main thread loop, run the server script infinitely 
-asyncio.get_event_loop().run_until_complete(server)
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    print("[ i ] A user connected.")
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    print("[ i ] A user disconnected.")
+
+
+async def server(websocket, path):
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = message.split("\n")
+            if data[0] == "<%ps>":
+                print("[ i ] Update public stream:", str(data[1]))
+                STREAMS["ps"] = str(data[1])
+                await notify_state()
+            elif data[0] == "<%ds>":
+                await unregister(websocket)
+            else:
+                print("[ ! ] Error: Unknown command:", str(data))
+    except:
+        await unregister(websocket)
+
+print("MikeDEV's CloudLink Server v1.6b\nNow listening for requests on port 3000.\n")
+cl_server = websockets.serve(server, "localhost", 3000)
+
+asyncio.get_event_loop().run_until_complete(cl_server)
 asyncio.get_event_loop().run_forever()
