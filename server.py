c5153646207e1f5ed959d488470e17c8fa4f885f
MikeDEV <mikierules109@gmail.com>
2020-07-09 01:42:56 -0400

    Update 1.6

--- server.py
+++ server.py
@@ -11,7 +11,8 @@ import json
 #Primary script that does all of our magic...
 async def client(websocket, path):
     global dStream
-    while True:
+    await websocket.send(json.dumps(dStream)) # Update the new client with the latest data
+    while True: # Loop forever, relay data between clients
         try:
             data = await websocket.recv()
             print(str(data))
@@ -20,9 +21,10 @@ async def client(websocket, path):
         except:
             pass
 
+# Print version and info :)
 print("MikeDEV's CloudLink Server v1.6\nNow listening for requests on port 3000.\n")
 server = websockets.serve(client, '127.0.0.1', 3000)
 
-while True:
-    asyncio.get_event_loop().run_until_complete(server)
-    asyncio.get_event_loop().run_forever()
+# Main thread loop, run the server script infinitely 
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
