9642cda507dc58096eb324c3fe7298a5a9abc1e7
MikeDEV <mikierules109@gmail.com>
2021-06-16 23:32:18 -0400

    Delete src/Link directory

--- src/Link/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from cloudlink import CloudLink
-
-if __name__ == "__main__":
-    cl = CloudLink() # Initialize CloudLink
-    cl.host(3000) # Run websocket server on ws://localhost:3000/
