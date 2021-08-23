b11242176b5214eb784c9e80728af858fc4be790
MikeDEV <mikierules109@gmail.com>
2021-08-22 21:30:45 -0400

    Update server_example.py

--- server_example.py
+++ server_example.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     cl = CloudLink() # Instanciate the module
     try:
         cl.host(3000) # Start the module in server mode, host on port 3000
+        cl.setMOTD("Hello, World!") # Set our Message-of-the-day
         
         while cl.mode == 1: # Some other spaghetti code to keep the script running while the connection is live
             time.sleep(0.1)
