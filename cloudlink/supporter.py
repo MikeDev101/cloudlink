b263ec1b1174512f75d8a7e0f94c4ab86f207a56
ShowierData9978 <mellfang36@gmail.com>
2023-01-12 10:48:23 -0600

    Use logging as a lib properly

--- cloudlink/supporter.py
+++ cloudlink/supporter.py
@@ -8,7 +8,7 @@ class supporter:
         self.parent = parent
         
         # Use logging library
-        self.logger = logging
+        self.logger = logging.getLogger("Cloudlink")
         
         # Reconfigure when needed
         self.logger.basicConfig(format="[%(asctime)s | %(created)f] (%(thread)d - %(threadName)s) %(levelname)s: %(message)s", level=self.logger.INFO)
